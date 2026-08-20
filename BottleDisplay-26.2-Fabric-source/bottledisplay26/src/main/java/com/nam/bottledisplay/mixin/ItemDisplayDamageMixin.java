a0563eed0f999d6b24a259a7fe29ec6608f92e7b
thuyenn1006-star <thuyenn1006@gmail.com>
2026-08-20 16:59:55 +0700

    Update ItemDisplayDamageMixin.java

--- BottleDisplay-26.2-Fabric-source/bottledisplay26/src/main/java/com/nam/bottledisplay/mixin/ItemDisplayDamageMixin.java
+++ BottleDisplay-26.2-Fabric-source/bottledisplay26/src/main/java/com/nam/bottledisplay/mixin/ItemDisplayDamageMixin.java
@@ -1,22 +1,20 @@
 package com.nam.bottledisplay.mixin;
 
 import com.nam.bottledisplay.BottleDisplayMod;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Display.ItemDisplay;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ItemDisplay.class)
-public abstract class ItemDisplayDamageMixin {
-    @Inject(method = "hurtServer", at = @At("HEAD"), cancellable = true)
-    private void bottledisplay$hurt(ServerPlayer player, DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+@Mixin(Entity.class)
+public class ItemDisplayDamageMixin {
+    @Inject(method = "hurt", at = @At("HEAD"), cancellable = true)
+    private void onHurt(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
         Entity self = (Entity) (Object) this;
         if (self.getTags().contains(BottleDisplayMod.TAG)) {
-            BottleDisplayMod.removeAndGive(self, player);
+            BottleDisplayMod.removeBottle(self);
             cir.setReturnValue(true);
         }
     }
