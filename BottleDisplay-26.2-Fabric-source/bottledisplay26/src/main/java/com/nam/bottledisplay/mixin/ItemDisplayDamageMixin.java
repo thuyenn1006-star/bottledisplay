4a086a879ae1fa2765edc57769bbaada9d3fde67
thuyenn1006-star <thuyenn1006@gmail.com>
2026-08-20 17:04:02 +0700

    Update ItemDisplayDamageMixin.java

--- BottleDisplay-26.2-Fabric-source/bottledisplay26/src/main/java/com/nam/bottledisplay/mixin/ItemDisplayDamageMixin.java
+++ BottleDisplay-26.2-Fabric-source/bottledisplay26/src/main/java/com/nam/bottledisplay/mixin/ItemDisplayDamageMixin.java
@@ -1,6 +1,7 @@
 package com.nam.bottledisplay.mixin;
 
 import com.nam.bottledisplay.BottleDisplayMod;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,8 +14,9 @@ public class ItemDisplayDamageMixin {
     @Inject(method = "hurt", at = @At("HEAD"), cancellable = true)
     private void onHurt(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
         Entity self = (Entity) (Object) this;
-        if (self.getTags().contains(BottleDisplayMod.TAG)) {
-            BottleDisplayMod.removeBottle(self);
+        if (self.getScoreboardTags().contains(BottleDisplayMod.TAG)) {
+            ServerPlayer player = source.getEntity() instanceof ServerPlayer p ? p : null;
+            BottleDisplayMod.removeAndGive(self, player);
             cir.setReturnValue(true);
         }
     }
